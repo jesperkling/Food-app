aa48b3217279dd676fd57382f3d4d411f28001f9
jesper kling <jesperkling@outlook.com>
2022-10-04 23:06:46 +0200

    Added pages and a navigation component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { Routes, Route } from 'react-router-dom'
+import Navigation from './components/Navigation'
+import HomePage from './pages/HomePage'
+import NotFound from './pages/NotFound'
+import './assets/scss/App.scss'
+
 function App() {
   return (
     <div className="App">
-      
+      <Navigation />
+
+      <Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
     </div>
   )
 }
