de0fa5cc03dba295768a3be08fb02893548aec5f
jesper kling <jesperkling@outlook.com>
2022-10-04 23:52:13 +0200

    Added Google Maps API and can now show map in homepage.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,19 @@
-import Container from 'react-bootstrap/Container'
+import { GoogleMap, LoadScript } from "@react-google-maps/api"
+import '../assets/scss/map.scss'
 
 const HomePage = () => {
 	return (
-		<Container className="py-3">
-			<h1>Welcome!</h1>
-		</Container>
+		<LoadScript googleMapsApiKey={`${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}`}>
+			<GoogleMap
+				zoom={12}
+				center={{
+					lat: 55.60,
+					lng: 13
+				}}
+				mapContainerClassName='map-container'
+			>
+			</GoogleMap>
+		</LoadScript>
 	)
 }
 
