6f513da849b82b24d28795be7d8db4ad830addd6
jesper kling <jesperkling@outlook.com>
2022-10-05 18:53:16 +0200

    Added create new place page as well as added link to it in the navigation.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -13,6 +13,7 @@ const Navigation = () => {
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="ms-auto">
 						<Nav.Link as={NavLink} end to="/places">Places</Nav.Link>
+						<Nav.Link as={NavLink} end to="/create-place">Add Place</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
