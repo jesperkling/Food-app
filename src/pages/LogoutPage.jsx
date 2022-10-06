2c736a4cde7f9e53c403b88b045ad7b72000c922
jesper kling <jesperkling@outlook.com>
2022-10-07 00:46:02 +0200

    Added logout component.

--- src/pages/LogoutPage.jsx
+++ src/pages/LogoutPage.jsx
@@ -1,34 +1,9 @@
-import React, { useEffect } from 'react'
-import { Container, Row, Col, Card } from 'react-bootstrap'
-import { useNavigate } from 'react-router-dom'
-import { useAuthContext } from '../contexts/AuthContext'
+import LogoutForm from '../components/LogoutForm'
 
 const LogoutPage = () => {
-    const navigate = useNavigate()
-    const { logout } = useAuthContext()
-
-    useEffect(() => {
-        const logoutUser = async () => {
-            await logout()
-            navigate('/')
-        }
-        logoutUser()
-    }, [])
-
     return (
-        <Container>
-            <Row>
-                <Col>
-                    <Card>
-                        <Card.Body>
-                            <Card.Title>Logout</Card.Title>
-                            <Card.Text>You are now logging out</Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-        </Container>
-    )
+        <LogoutForm />    
+    )    
 }
 
 export default LogoutPage
