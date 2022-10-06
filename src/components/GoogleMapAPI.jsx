92d0f6c53336e4185e3bc2407d1bfb791e6c6ee3
jesper kling <jesperkling@outlook.com>
2022-10-06 21:49:27 +0200

    Handling if map is loaded.

--- src/components/GoogleMapAPI.jsx
+++ src/components/GoogleMapAPI.jsx
@@ -1,30 +1,30 @@
-import { GoogleMap, useJsApiLoader, Autocomplete } from "@react-google-maps/api"
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api"
 import { useState } from "react"
 import '../assets/scss/map.scss'
 
 const libraries = ['places']
 
 const GoogleMapAPI = () => {
-    const { isLoaded, loadError } = useJsApiLoader({
+    const { isLoaded } = useJsApiLoader({
         googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
         libraries
     })
 
     const [latitude, setLatitude] = useState(55.60)
     const [longitude, setLongitude] = useState(13)
-    const [adress, setAdress] = useState('')
 
     return (
-        <>
+        isLoaded && (
             <GoogleMap
-                zoom={12}
-				center={{
-					lat: 55.60,
-					lng: 13
-				}}
-				mapContainerClassName='map-container'
+            zoom={12}
+			center={{
+				lat: latitude,
+				lng: longitude
+			}}
+			mapContainerClassName='map-container'
             ></GoogleMap>
-        </>
+        )
+        
     )
 }
 
