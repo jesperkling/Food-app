e36ab2f920eb7ee0cee9395aa0eb1260815f912c
jesper kling <jesperkling@outlook.com>
2022-10-07 00:37:48 +0200

    Fixed Google Maps API code.

--- src/components/GoogleMapAPI.jsx
+++ src/components/GoogleMapAPI.jsx
@@ -1,31 +1,30 @@
 import { GoogleMap, useJsApiLoader } from "@react-google-maps/api"
-import { useState } from "react"
 import '../assets/scss/map.scss'
-
-const libraries = ['places']
+import { useState } from "react"
 
 const GoogleMapAPI = () => {
-    const { isLoaded } = useJsApiLoader({
+    const { isLoaded, loadError } = useJsApiLoader({
         googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
-        libraries
     })
 
     const [latitude, setLatitude] = useState(55.60)
     const [longitude, setLongitude] = useState(13)
 
-    return (
-        isLoaded && (
-            <GoogleMap
-            zoom={12}
-			center={{
-				lat: latitude,
-				lng: longitude
-			}}
-			mapContainerClassName='map-container'
-            ></GoogleMap>
+    if (isLoaded) {
+        return (
+            isLoaded && (
+                <GoogleMap
+                zoom={12}
+                center={{
+                    lat: latitude,
+                    lng: longitude
+                }}
+                mapContainerClassName='map-container'
+                ></GoogleMap>
+            )
+            
         )
-        
-    )
+    }
 }
 
 export default GoogleMapAPI
